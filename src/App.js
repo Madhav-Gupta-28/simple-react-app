baad434d87858d401da0535c77d8177ed92c6a44
Madhav-Gupta-28 <guptamadhav2828@gmail.com>
2023-04-24 22:08:21 +0530

    app is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/App.scss';
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './components/Home'
+import Headers from './components/Header'
+import Footer from './components/Footer';
+import Services from './components/Services';
+import Contact from './components/Contact';
+
+import "./styles/Header.scss"
+import"./styles/Home.scss"
+import "./styles/contact.scss"
+import "./styles/Footer.scss"
+import "./styles/mediaquery.scss"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Headers/>
+    <Routes>
+    <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
